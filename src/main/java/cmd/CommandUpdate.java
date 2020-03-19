a76d64ce54c6a69a5a03b49e63ec1afa5350256d
OwamouS <alexandrlmashkovtsev@niuitmo.ru>
2020-03-19 18:28:47 +0300

    command update id

--- src/main/java/cmd/CommandUpdate.java
+++ src/main/java/cmd/CommandUpdate.java
@@ -8,13 +8,26 @@ import productdata.Product;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.Map;
 
 public class CommandUpdate implements Command {
 
     @Override
     public void execute(String[] args) {
-        for(int i = 0;i<TableController.getCurrentTable().getSize();i++) {
-
+        int counter = 0;
+        Iterator<Map.Entry<String, Product>> it = TableController.getCurrentTable().getSet().iterator();
+        int i = Integer.parseInt(args[0]);
+        while(it.hasNext()){
+            Map.Entry<String,Product> map = it.next();
+            if (map.getValue().getId() == i) {
+                counter ++;
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                TableController.getCurrentTable().replace(map.getKey(),new Product(reader));
+            }
+        }
+        if(counter==0){
+            System.out.println("Элемента с данным id не существует. Повторите ввод: ");
         }
     }
 
