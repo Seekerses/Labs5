e4d927cbe0e42a229006e978258d2f21b3c4cf6a
Daniil <seekergodlike@gmail.com>
2020-03-26 13:48:21 +0300

    bugfix and translation

--- src/main/java/cmd/CommandHelp.java
+++ src/main/java/cmd/CommandHelp.java
@@ -11,22 +11,22 @@ public class CommandHelp implements Command {
     @Override
     public void execute(String[] args){
 
-        System.out.println("help : вывести справку по доступным командам\n" +
-                "info : вывести в стандартный поток вывода информацию о коллекции (тип, дата инициализации, количество элементов и т.д.)\n" +
-                "show : вывести в стандартный поток вывода все элементы коллекции в строковом представлении\n" +
-                "insert key {element} : добавить новый элемент с заданным ключом\n" +
-                "update id {element} : обновить значение элемента коллекции, id которого равен заданному\n" +
-                "remove_key key : удалить элемент из коллекции по его ключу\n" +
-                "clear : очистить коллекцию\n" +
-                "save : сохранить коллекцию в файл\n" +
-                "execute_script file_name : считать и исполнить скрипт из указанного файла. В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.\n" +
-                "exit : завершить программу (без сохранения в файл)\n" +
-                "remove_lower {element} : удалить из коллекции все элементы, меньшие, чем заданный\n" +
-                "history : вывести последние 7 команд (без их аргументов)\n" +
-                "replace_if_greater key {element} : заменить значение по ключу, если новое значение больше старого\n" +
-                "min_by_name : вывести любой объект из коллекции, значение поля name которого является минимальным\n" +
-                "group_counting_by_coordinates : сгруппировать элементы коллекции по значению поля coordinates, вывести количество элементов в каждой группе\n" +
-                "filter_less_than_manufacturer manufacturer : вывести элементы, значение поля manufacturer которых меньше заданного");
+        System.out.println("help: display help for available commands \n " +
+                "info: display information about the collection (type, initialization date, number of elements, etc.) in the standard output stream \n" +
+                "show: output to the standard output stream all the elements of the collection in the string representation \n" +
+                "insert key {element}: add a new element with the given key \n" +
+                "update id {element}: update the value of a collection element whose id is equal to the given \n" +
+                "remove_key key: remove an item from the collection by its key \n" +
+                "clear: clear collection \n" +
+                "save: save collection to file \n" +
+                "execute_script file_name: read and execute the script from the specified file. The script contains the commands in the same form in which the user types them interactively. \n" +
+                "exit: exit the program (without saving to a file) \n" +
+                "remove_lower {element}: remove from the collection all elements smaller than the specified \n" +
+                "history: print the last 7 commands (without their arguments) \n" +
+                "replace_if_greater key {element}: replace the value by key, if the new value is greater than the old \n" +
+                "min_by_name: output any object from the collection whose name field value is minimal \n" +
+                "group_counting_by_coordinates: group the elements of the collection by the value of the coordinates field, display the number of elements in each group \n" +
+                "filter_less_than_manufacturer manufacturer: display elements whose manufacturer field value is less than the specified");
     }
 
     /**
