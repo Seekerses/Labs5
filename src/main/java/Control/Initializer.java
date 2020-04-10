bc05d1d3cbe394c5ada3da6cf175bb9f44ab8cbc
Daniil <seekergodlike@gmail.com>
2020-04-10 12:12:16 +0300

    fixes 2

--- src/main/java/Control/Initializer.java
+++ src/main/java/Control/Initializer.java
@@ -76,43 +76,50 @@ public class Initializer {
      */
     public static Product build(TableManager table, String[] str){
         Location location;
-        if (!"".equals(str[7] +  str[9])){
-            location = new Location(Long.parseLong(str[7]),"".equals(str[8])? 0 : Integer.parseInt(str[8]), Long.parseLong(str[9]));
-        }
-        else{
-            location = null;
-        }
-        Address adr;
-        if (!"".equals(str[6])) {
-            adr = new Address(str[6], location);
-        } else {
-             adr = null;
-        }
-        Product product = null;
         try {
-            Coordinates coord = new Coordinates("".equals(str[3]) ? 0 : Double.parseDouble(str[3]), Integer.parseInt(str[4]));
-            Organization org;
-            if(!"".equals(str[11])) {
-                try {
-                    org = new Organization("".equals(str[5]) ? null : Integer.parseInt(str[5]), str[10], str[11], "".equals(str[12]) ? null : OrganizationType.valueOf(str[12]), adr);
-                }
-                catch (NotUniqueFullName e){
-                    Organization comp = UniqueController.getOrgTable().get(str[11]);
-                    if(comp.getPostalAddress().equals(adr) && comp.getName().equals(str[10])
-                            && comp.getType().equals("".equals(str[12]) ? null : OrganizationType.valueOf(str[12])))
-                        org = comp;
-                    else throw new NotUniqueFullName();
-                }
+            if (!"".equals(str[7] + str[9])) {
+                location = new Location(Long.parseLong(str[7]), "".equals(str[8]) ? 0 : Integer.parseInt(str[8]), Long.parseLong(str[9]));
+            } else {
+                location = null;
             }
-            else {org = null;}
-            product = new Product("".equals(str[1])? null:Long.parseLong(str[1]),str[2], coord, str.length < 16 ? null : Float.parseFloat(str[15]), UnitOfMeasure.valueOf(str[13]), org , "".equals(str[14]) ? null: LocalDateTime.parse(str[14]));
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
+            Address adr;
+            if (!"".equals(str[6])) {
+                adr = new Address(str[6], location);
+            } else {
+                adr = null;
+            }
+            Product product = null;
+            try {
+                Coordinates coord = new Coordinates("".equals(str[3]) ? 0 : Double.parseDouble(str[3]), Integer.parseInt(str[4]));
+                Organization org;
+                if (!"".equals(str[11])) {
+                    try {
+                        org = new Organization("".equals(str[5]) ? null : Integer.parseInt(str[5]), str[10], str[11], "".equals(str[12]) ? null : OrganizationType.valueOf(str[12]), adr);
+                    } catch (NotUniqueFullName e) {
+                        Organization comp = UniqueController.getOrgTable().get(str[11]);
+                        if (comp.getPostalAddress().equals(adr) && comp.getName().equals(str[10])
+                                && comp.getType().equals("".equals(str[12]) ? null : OrganizationType.valueOf(str[12])))
+                            org = comp;
+                        else throw new NotUniqueFullName();
+                    }
+                } else {
+                    org = null;
+                }
+                product = new Product("".equals(str[1]) ? null : Long.parseLong(str[1]), str[2], coord, str.length < 16 ? null : Float.parseFloat(str[15]), UnitOfMeasure.valueOf(str[13]), org, "".equals(str[14]) ? null : LocalDateTime.parse(str[14]));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         if (product != null) {
             table.put(str[0], product);
         }
         return product;
+        }
+        catch (Exception e){
+            System.out.println("Wrong arguments. Argument should be in format \"key productId productName xCoordinate yCoordinate organizationId" +
+                    "orgStreet xOrgCoordinate yOrgCoordinate zOrgCoordinate organizationName organizationFullName orgType UnitsOfMeasure creationDate price\"" +
+                    "to create null value use ;. In files separator is \";\" instead of \" \" and null value is \"\".");
+        return null;
+    }
     }
 }
